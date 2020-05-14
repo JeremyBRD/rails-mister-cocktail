15b7dff0bf7f5023f6ade65bc0d46f5eaefe3a2a
Jeremy Barbedienne <jeremybarbedienne@gmail.com>
2020-05-14 14:59:18 +0200

    add controller and views for cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,30 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktails.all
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
